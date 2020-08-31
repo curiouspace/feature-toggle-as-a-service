17e1f93893d989178e553bda7f32aefb94e7d453
Prajwal Das <prajwaldas.dev@gmail.com>
2020-08-31 21:14:23 +0530

    Sample Client UI

---
+++ ft-samples/ft-sample-client-02/src/main/java/org/ft/client/controller/FeatureToggleController.java
@@ -0,0 +1,28 @@
+package org.ft.client.controller;
+
+import lombok.AllArgsConstructor;
+import org.ft.client.service.FeatureToggleService;
+import org.ft.core.response.FeatureToggleResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Prajwal Das
+ */
+@Controller
+@AllArgsConstructor
+@RequestMapping("/ui/features")
+public class FeatureToggleController
+{
+    FeatureToggleService featureToggleService;
+
+    @GetMapping
+    public String getFeatureInfo(@RequestParam String tenant, Model model) {
+        FeatureToggleResponse featureInfo = featureToggleService.getAllFeatureToggles(tenant);
+        model.addAttribute("featureToggles", featureInfo.getFeatures());
+        return "index";
+    }
+}
