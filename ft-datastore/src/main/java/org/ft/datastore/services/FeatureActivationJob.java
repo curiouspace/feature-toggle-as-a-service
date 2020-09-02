b4e1cde8f820eb9f6f21089829c0c68fb4670bd3
Prajwal Das <prajwaldas.dev@gmail.com>
2020-09-02 20:24:24 +0530

    Used native query to update feature status

--- ft-datastore/src/main/java/org/ft/datastore/services/FeatureActivationJob.java
+++ ft-datastore/src/main/java/org/ft/datastore/services/FeatureActivationJob.java
@@ -3,7 +3,6 @@ package org.ft.datastore.services;
 import lombok.AllArgsConstructor;
 import org.ft.core.services.FeatureActivator;
 import org.ft.core.services.FeatureDataStore;
-import org.ft.datastore.models.Feature;
 import org.ft.datastore.repository.FeatureToggleRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -25,12 +24,8 @@ public class FeatureActivationJob implements FeatureActivator
     public void activate ()
     {
         LocalDate localDate = LocalDate.now();
-        List<String> allTenantIds = rdbTenantStore.getAllTenantIds();
-        allTenantIds.forEach(it ->{
-            List<Feature> featureList = featureToggleRepository.getFeatureByActivationDate(localDate);
-            for (Feature f : featureList) {
-                featureDataStore.enable(f.getId(), it);
-            }
-        });
+        List<String> featureList = featureToggleRepository.getFeatureIdsByActivationDate(
+            localDate);
+        featureDataStore.enableForAll(featureList);
     }
 }
